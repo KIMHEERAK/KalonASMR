6b2e43236aaf153207a1d0f311fb3a56431f92cd
novis <khrock89@naver.com>
2017-01-31 15:14:49 +0900

    gitUpLoad Test - MainActivity, layout-activity_main

--- app/src/main/java/www/kalon/com/kalonasmr/MainActivity.java
+++ app/src/main/java/www/kalon/com/kalonasmr/MainActivity.java
@@ -21,49 +21,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main_tab);
-
-        MobileAds.initialize(getApplicationContext(), getString(R.string.banner_ad_unit_id_choice));
-
-        AdView mAdView = (AdView) findViewById(R.id.adView);
-
-//        AdRequest adRequest = new AdRequest.Builder()
-//                .addTestDevice(AdRequest.DEVICE_ID_EMULATOR)        // All emulators
-//                .addTestDevice("355325074727480")  // An example device ID
-//                .build();
-
-        AdRequest adRequest = new AdRequest.Builder().build();
-        mAdView.loadAd(adRequest);
-
-        //MediaPlayer
-        mMediaPlayerConan = MediaPlayer.create(this, R.raw.conan);
-        mMediaPlayerRain = MediaPlayer.create(this, R.raw.rain);
-
-        // TabLayout
-        TabLayout tabLayout = (TabLayout) findViewById(R.id.tab_layout);
-        tabLayout.setTabGravity(TabLayout.GRAVITY_FILL);
-
-        final ViewPager viewPager = (ViewPager) findViewById(R.id.pager);
-        final PageAdapter adapter = new PageAdapter
-                (getSupportFragmentManager(), tabLayout.getTabCount());
-        viewPager.setAdapter(adapter);
-        viewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
-        tabLayout.setOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
-            @Override
-            public void onTabSelected(TabLayout.Tab tab) {
-                viewPager.setCurrentItem(tab.getPosition());
-            }
-
-            @Override
-            public void onTabUnselected(TabLayout.Tab tab) {
-
-            }
-
-            @Override
-            public void onTabReselected(TabLayout.Tab tab) {
-
-            }
-        });
+        setContentView(R.layout.activity_main);
 
 
     } // onCreate
@@ -97,7 +55,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onDestroy() {
         super.onDestroy();
 
-        mMediaPlayerConan.release();
 
     }
 
